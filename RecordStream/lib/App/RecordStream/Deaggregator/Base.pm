a2ac333935182fe2932f9ab14a0fac8dbf7470e5
Ben Bernard <github@benjaminbernard.com>
2011-10-03 15:39:15 -0700

    Removing RecordStream

--- RecordStream/lib/App/RecordStream/Deaggregator/Base.pm
+++
@@ -1,5 +0,0 @@
-package App::RecordStream::Deaggregator::Base;
-
-# marker only
-
-1;
--- RecordStream/src/fast-recs-collate/lookup3.h
+++
@@ -1,5 +0,0 @@
-
-#include <stdint.h>     /* defines uint32_t etc */
-
-extern uint32_t hashlittle( const void *key, size_t length, uint32_t initval);
-
