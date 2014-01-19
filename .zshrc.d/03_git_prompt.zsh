0ca112f3ecc921ce4d58b445300c5be223bf1e14
Ben Bernard <github@benjaminbernard.com>
2014-01-19 00:13:39 -0800

    Various updates to home directory

--- .zshrc.d/03_git_prompt.zsh
+++ .zshrc.d/03_git_prompt.zsh
@@ -1,60 +1,66 @@
-## http://www.jonmaddox.com/2008/03/13/show-your-git-branch-name-in-your-prompt/
-#function git_current_branch () {
-#  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-#}
-#
-## git status with a dirty flag
-#function git_status_flag () {
-#  git_status="$(git status 2>/dev/null)"
-#  remote_pattern="# Your branch is .* by .* commit"
-#  diverge_pattern="# Your branch and .* have diverged"
-#
-#  if [[ ${git_status} =~ "working directory clean" ]]; then
-#    state=""
-#  else
-#    state="*"
-#  fi
-#
-#  if [[ ${git_status} =~ ${remote_pattern} ]]; then
-#    if [[ ${git_status} =~ "ahead" ]]; then
-#      remote="↑"
-#    else
-#      remote="↓"
-#    fi
-#  fi
-#
-#  if [[ ${git_status} =~ ${diverge_pattern} ]]; then
-#    remote="↕"
-#  fi
-#
-#  echo "${state}${remote}"
-#}
-#
-#function git_prompt_decorations () {
-#    local branch=`git_current_branch`
-#    local git_status=`git_status_flag`
-#    if [[ ! -z $branch ]]; then
-#        echo -en "[${branch}${git_status}] "
-#    fi
-#}
+# Adapted from: https://gist.github.com/wolever/6525437
+# 100% pure Bash (no forking) function to determine the name of the current git branch
+function gitbranch() {
+    export GITBRANCH=""
 
-#preexec () { 
-#  # End the colorization of the line
-#  echo -n '\e[0m' 
-#
-#  # Coped from envImprovement zshrc, sets the command as the
-#  # screen title
-#  if [[ "$TERM" == "screen" ]]; then
-#    local CMD=${1/% */}  # kill all text after and including the first space
-#    echo -ne "\ek$CMD\e\\"
-#  fi
-#
-#  #git_branch=`git_prompt_decorations`
-#  #PS1="$git_branch$MY_PROMPT"
-#}
+    local repo="${_GITBRANCH_LAST_REPO-}"
+    local gitdir=""
+    [[ ! -z "$repo" ]] && gitdir="$repo/.git"
 
-#These lines are supposed to initialize the SC info for the prompt, but don't
-#seem to be working
-#autoload -Uz vcs_info
-#autoload -Uz vcs_info_printsys
+    # If we don't have a last seen git repo, or we are in a different directory
+    if [[ -z "$repo" || "$PWD" != "$repo"* || ! -e "$gitdir" ]]; then
+        local cur="$PWD"
+        while [[ ! -z "$cur" ]]; do
+            if [[ -e "$cur/.git" ]]; then
+                repo="$cur"
+                gitdir="$cur/.git"
+                break
+            fi
+            cur="${cur%/*}"
+        done
+    fi
 
+    if [[ -z "$gitdir" ]]; then
+        unset _GITBRANCH_LAST_REPO
+        return 0
+    fi
+    export _GITBRANCH_LAST_REPO="${repo}"
+
+    echo $(parsehead $gitdir)
+}
+
+function parsehead() {
+    local head=""
+    local branch=""
+    local gitdir=$1
+
+    if [[ -d $gitdir ]]; then
+      read head < "$gitdir/HEAD"
+      case "$head" in
+          ref:*) #normal ref
+              branch="${head##*/}"
+              ;;
+          "")
+              branch=""
+              ;;
+          *) # Detached head
+              branch="${head:0:7}"
+              ;;
+      esac
+    elif [[ -e $gitdir && $2 != "NO" ]]; then
+      # covers sub modules
+      read head < "$gitdir"
+      case "$head" in
+        gitdir:*)
+          newdir=$(echo $head | cut -d' ' -f2)
+          ;;
+      esac
+      echo $(parsehead $newdir "NO")
+    fi
+
+    if [[ -z "$branch" ]]; then
+        return 0
+    fi
+    export GITBRANCH="$branch"
+    echo ${repo##*/}:$GITBRANCH
+}
