e63a8e282f02bd1e1f0ca8b3b696fa85d92b977c
Ben Bernard <github@benjaminbernard.com>
2014-03-22 18:40:59 -0700

    Updates!

--- .zshrc.d/02_environment.zsh
+++ .zshrc.d/02_environment.zsh
@@ -5,8 +5,10 @@ setenv SCREENDIR ~/.screen         # directory to put screen sockets
 setenv URLOPENER_LOG_FILE $HOME/.urlopener.log # Have the url opener log
 setenv LS_COLORS 'no=00:fi=00:di=01;34:ln=01;31:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:*.tex=01;33:*.sxw=01;33:*.sxc=01;33:*.lyx=01;33:*.pdf=0;35:*.ps=00;36:*.asm=1;33:*.S=0;33:*.s=0;33:*.h=0;31:*.c=0;35:*.cxx=0;35:*.cc=0;35:*.C=0;35:*.o=1;30:*.am=1;33:*.py=0;34:'
 
-export PATH=/usr/local/bin:$PATH:$HOME/bin
+export PATH=/usr/local/bin:/usr/local/opt/coreutils/libexec/gnubin:$PATH:$HOME/bin
 export VIM_TEMP="/var/tmp/$USER/vim-temp"
+
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 if [[ ! -d $VIM_TEMP ]]; then
   mkdir -p $VIM_TEMP
