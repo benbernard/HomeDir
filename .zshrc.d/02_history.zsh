c4bfd372fa3e938f8e74ec75c0d46c36b4264331
Ben Bernard <github@benjaminbernard.com>
2014-05-02 18:10:10 -0700

    Updates
    1. Update soem submodules
    2. Add vim-markdown
    3. Move unite config to its own vim file
    4. Fix up paste-tracker
    5. Setup screen startup script

--- .zshrc.d/02_history.zsh
+++ .zshrc.d/02_history.zsh
@@ -3,6 +3,7 @@ HISTSIZE=400000
 SAVEHIST=410000
 HISTFILE=${HOME}/.history
 
-setopt INC_APPEND_HISTORY # immediatly insert history into history file
-setopt SHARE_HISTORY      # share history between all shells...don't know if this is a good idea or not... oh it is...
-setopt HIST_IGNORE_DUPS   # don't insert immediately duplicated lines twice
+setopt INC_APPEND_HISTORY  # immediatly insert history into history file
+setopt SHARE_HISTORY       # share history between all shells...don't know if this is a good idea or not... oh it is...
+setopt HIST_IGNORE_DUPS    # don't insert immediately duplicated lines twice
+setopt NOHIST_IGNORE_SPACE # Turn off stupid option set by oh-my-zsh that doesn't add to history commands that started with a space
