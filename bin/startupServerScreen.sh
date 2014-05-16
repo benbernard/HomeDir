bcbb82262bc2edf74cde297c5e093f422144505a
Ben Bernard <github@benjaminbernard.com>
2014-05-16 10:31:43 -0700

    Updating for one more server, protect against dir

--- bin/startupServerScreen.sh
+++ bin/startupServerScreen.sh
@@ -6,9 +6,11 @@ screen
 screen
 screen
 screen
+screen
 
-screen -p 0 -X stuff "foreman start"
-screen -p 1 -X stuff "grunt observe"
+screen -p 0 -X stuff "cdcl; foreman start"
+screen -p 1 -X stuff "cdcl; grunt observe"
 screen -p 2 -X stuff "node-inspector"
 screen -p 3 -X stuff "java -jar /Users/bernard/bin/selenium-server-standalone-2.38.0.jar"
 screen -p 4 -X stuff "cd; cd bin/mac; ./paste-tracker.pl"
+screen -p 5 -X stuff "cd; cd mongo-edit; node server.js config/fieldbook.js"
