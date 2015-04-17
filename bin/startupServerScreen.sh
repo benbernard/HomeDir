e28cabc427328f6ac6109aa66bf404e34cba34c1
Ben Bernard <github@benjaminbernard.com>
2015-04-17 13:06:47 -0700

    Add worker screen

--- bin/startupServerScreen.sh
+++ bin/startupServerScreen.sh
@@ -16,11 +16,13 @@ screen
 screen
 screen
 screen
+screen
 
-screen -p 0 -X stuff "cdcl; foreman start"
-screen -p 1 -X stuff "cdcl; grunt observe"
-screen -p 2 -X stuff "cdcl; ~/.nvm/v0.10.21/bin/node-inspector --hidden 'node_modules'"
-screen -p 3 -X stuff "java -jar /Users/bernard/bin/selenium-server-standalone-2.38.0.jar"
-screen -p 4 -X stuff "cd; cd bin/mac; ./paste-tracker.pl"
-screen -p 5 -X stuff "cd; genghisapp -L -F"
-screen -p 6 -X stuff "AUTOSSH_POLL=30 AUTOSSH_DEBUG=1 autossh -M2000 -L6667:localhost:6667 -N cmyers.org"
+screen -p 0 -X stuff "cdcl; foreman start web"
+screen -p 1 -X stuff "cdcl; foreman start worker"
+screen -p 2 -X stuff "cdcl; grunt observe"
+screen -p 3 -X stuff "cdcl; ~/.nvm/v0.10.21/bin/node-inspector --hidden 'node_modules'"
+screen -p 4 -X stuff "java -jar /Users/bernard/bin/selenium-server-standalone-2.38.0.jar"
+screen -p 5 -X stuff "cd; cd bin/mac; ./paste-tracker.pl"
+screen -p 6 -X stuff "cd; genghisapp -L -F"
+screen -p 7 -X stuff "AUTOSSH_POLL=30 AUTOSSH_DEBUG=1 autossh -M2000 -L6667:localhost:6667 -N cmyers.org"
