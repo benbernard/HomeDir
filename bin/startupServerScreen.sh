755df0081b75fb2054d5927bc812b10495fda464
Ben Bernard <github@benjaminbernard.com>
2016-07-05 15:42:55 -0700

    Better server screen startups

--- bin/startupServerScreen.sh
+++ bin/startupServerScreen.sh
@@ -8,9 +8,11 @@ then
   exit 1;
 fi
 
-tmux set-option -t server allow-rename off
+# First turn off window rename (need global option because of race conditions
+# with shell re-titling)
+tmux set-option -g allow-rename off
 
-# This is total jank, but gets what I want done
+# Setup windows, note we rename the current window instead of making all new
 tmux rename-window web
 
 tmux new-window -n worker
@@ -20,6 +22,18 @@ tmux new-window -n paste-tracker
 tmux new-window -n genghis
 tmux new-window -n autossh
 
+# Turn off rename on windows so that when we unset the global option it will be
+# set off on the windows
+tmux set-window-option -t web allow-rename off
+tmux set-window-option -t worker allow-rename off
+tmux set-window-option -t watch allow-rename off
+tmux set-window-option -t inspector allow-rename off
+tmux set-window-option -t paste-tracker allow-rename off
+tmux set-window-option -t genghis allow-rename off
+tmux set-window-option -t autossh allow-rename off
+
+# Startup server commands.  Use send-keys so I can flip to windows and
+# control-c from programs and get into a shell
 tmux send-keys -t web "cdcl; foreman start web" Enter
 tmux send-keys -t worker "cdcl; foreman start worker" Enter
 tmux send-keys -t watch "cdcl; while (1) { grunt min-watch }" Enter
@@ -27,3 +41,6 @@ tmux send-keys -t inspector "cdcl; node-inspector" Enter
 tmux send-keys -t paste-tracker "cd; cd bin/mac; ./paste-tracker.pl" Enter
 tmux send-keys -t genghis "cd; genghisapp -L -F" Enter
 tmux send-keys -t autossh "AUTOSSH_POLL=30 AUTOSSH_DEBUG=1 autossh -M2000 -L6667:localhost:6667 -N cmyers.org" Enter
+
+# Turn back on global window renames
+tmux set-option -g allow-rename on
