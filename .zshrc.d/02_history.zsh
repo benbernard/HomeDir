6d055352f06e1a258b2371f3f5346ed7770f708e
Ben Bernard <github@benjaminbernard.com>
2016-09-14 16:57:47 -0700

    Use HIST_IGNORE_SPACE

--- .zshrc.d/02_history.zsh
+++ .zshrc.d/02_history.zsh
@@ -6,4 +6,4 @@ HISTFILE=${HOME}/.history
 setopt INC_APPEND_HISTORY  # immediatly insert history into history file
 setopt SHARE_HISTORY       # share history between all shells...don't know if this is a good idea or not... oh it is...
 setopt HIST_IGNORE_DUPS    # don't insert immediately duplicated lines twice
-setopt NOHIST_IGNORE_SPACE # Turn off stupid option set by oh-my-zsh that doesn't add to history commands that started with a space
+setopt HIST_IGNORE_SPACE   # Don't save commands that start with a space to history
