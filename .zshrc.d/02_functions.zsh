1c09cac4813bf8ce1edc7d7ab4db659ff32008cd
Ben Bernard <github@benjaminbernard.com>
2017-01-25 09:29:20 -0800

    Add sslinfo function

--- .zshrc.d/02_functions.zsh
+++ .zshrc.d/02_functions.zsh
@@ -90,3 +90,7 @@ addVimPlugin() {
 nt() {
   tmux attach-session -t "$@" || tmux new-session -s "$@"
 }
+
+sslinfo () {
+  echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text
+}
