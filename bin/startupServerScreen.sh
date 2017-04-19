4db8fd8f5f3c87bda6cc2e12d8a7198adb73d3bf
Ben Bernard <github@benjaminbernard.com>
2017-04-19 16:40:57 -0700

    Do not use multi-pane, non-performant

--- bin/startupServerScreen.sh
+++ bin/startupServerScreen.sh
@@ -13,33 +13,39 @@ fi
 tmux set-option -g allow-rename off
 
 # Setup windows, note we rename the current window instead of making all new
-tmux rename-window servers
+tmux rename-window web
 
-tmux split-window -v
-tmux split-window -h
-tmux split-window -h -t 0
-
-tmux set-window-option -t servers allow-rename off
-
-tmux send-keys -t 0 'cdcl; foreman start web' Enter
-tmux send-keys -t 1 'cdcl; foreman start worker' Enter
-tmux send-keys -t 2 'cdcl; foreman start pgBridge' Enter
-tmux send-keys -t 3 'cdcl; foreman start valueCleaner' Enter
-
+tmux new-window -n worker
+tmux new-window -n pgBridge
+tmux new-window -n valueCleaner
 tmux new-window -n watch
+#tmux new-window -n inspector
 tmux new-window -n paste-tracker
+tmux new-window -n genghis
 tmux new-window -n autossh
 
 # Turn off rename on windows so that when we unset the global option it will be
 # set off on the windows
+tmux set-window-option -t web allow-rename off
+tmux set-window-option -t worker allow-rename off
+tmux set-window-option -t pgBridge allow-rename off
+tmux set-window-option -t valueCleaner allow-rename off
 tmux set-window-option -t watch allow-rename off
+#tmux set-window-option -t inspector allow-rename off
 tmux set-window-option -t paste-tracker allow-rename off
+tmux set-window-option -t genghis allow-rename off
 tmux set-window-option -t autossh allow-rename off
 
 # Startup server commands.  Use send-keys so I can flip to windows and
 # control-c from programs and get into a shell
+tmux send-keys -t web "cdcl; foreman start web" Enter
+tmux send-keys -t worker "cdcl; foreman start worker" Enter
+tmux send-keys -t pgBridge "cdcl; foreman start pgBridge" Enter
+tmux send-keys -t valueCleaner "cdcl; foreman start valueCleaner" Enter
 tmux send-keys -t watch "cdcl; while (1) { grunt min-watch }" Enter
+#tmux send-keys -t inspector "cdcl; node-inspector" Enter
 tmux send-keys -t paste-tracker "cd; cd bin/mac; ./paste-tracker.pl" Enter
+tmux send-keys -t genghis "cd; genghisapp -L -F" Enter
 tmux send-keys -t autossh "AUTOSSH_POLL=30 AUTOSSH_DEBUG=1 autossh -M2000 -L6667:localhost:6667 -N cmyers.org" Enter
 
 # Turn back on global window renames
