6bc878814bc9341904312ea1eb45384757803f85
Ben Bernard <github@benjaminbernard.com>
2017-07-27 10:56:58 -0700

    Adding cool git stuff with fzf

---
+++ .zshrc.d/09_fzf.zsh
@@ -0,0 +1,78 @@
+# Inspired by
+# - https://github.com/junegunn/fzf/wiki/Examples#git
+# - https://junegunn.kr/2016/07/fzf-git/
+
+# fco - checkout git branch/tag
+fzf_get_ref() {
+  local tags branches target
+
+  tags=$(
+    git for-each-ref --sort=-committerdate refs/tags/ --format="%(refname:short)" |
+    awk '{print "\x1b[31;1mtag\x1b[m\t" $1}'
+  ) || return
+
+  localBranches=$(
+    git for-each-ref --sort=-committerdate refs/heads/ --format="%(refname:short)" |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    awk '{print "\x1b[34;1mlocal\x1b[m\t" $1}'
+  ) || return
+
+  remoteBranches=$(
+    git for-each-ref --sort=-committerdate refs/remotes/ --format="%(refname:short)" |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    awk '{print "\x1b[34;1mremote\x1b[m\t" $1}'
+  ) || return
+
+  target=$(
+    (echo "$localBranches"; echo "$remoteBranches"; echo "$tags") |
+    fzf -m --ansi \
+      --preview-window=left \
+      --preview 'git log --pretty=oneline --abbrev-commit --color=always `echo {} | cut -f2` | head -'$LINES |
+    awk '{print $2}' |
+    xargs echo
+  )
+
+  echo $target
+}
+
+fco() {
+  local target=$(fzf_get_ref)
+  echo git checkout "${target}"
+}
+
+fzf_ref_widget() {
+  local refs=$(fzf_get_ref)
+  LBUFFER="${LBUFFER}${refs}"
+  local ret=$?
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  return $ret
+}
+
+zle -N fzf_ref_widget
+bindkey '^B' fzf_ref_widget
+
+# CTRL-G - Get git commit
+fzf-git-widget() {
+  setopt localoptions pipefail 2> /dev/null
+  local commits=$(
+    git log --pretty=oneline --abbrev-commit --color=always |
+    fzf -m --ansi --preview 'git show --color=always `echo {} | cut -d" " -f1` | head -'$LINES |
+    awk -F' ' '{print $1}' |
+    xargs echo
+  )
+
+  if [[ -z "$commits" ]]; then
+    zle redisplay
+    return 0
+  fi
+
+  LBUFFER="${LBUFFER}${commits}"
+  local ret=$?
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  return $ret
+}
+
+zle -N fzf-git-widget
+bindkey '^G' fzf-git-widget
