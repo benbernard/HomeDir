135e72137a714b2909e0cc4e6ccdc56a09cb9c70
Ben Bernard <github@benjaminbernard.com>
2018-07-19 16:40:30 -0700

    Updates for non-amling tmux version

--- .zshrc.d/00_setup_envimprovement.zsh
+++ .zshrc.d/00_setup_envimprovement.zsh
@@ -1,5 +1,6 @@
 # Sets: ENV_IMPROVEMENT_ROOT, ZSH_VERSION
 
+
 if [[ "$NINJA_SEARCH_SHELL" = "YES" ]]
 then
   AUTO_TITLE_SCREENS=NO
@@ -12,6 +13,8 @@ if [[ -n "$TITLE" ]]
 then
   AUTO_TITLE_SCREENS=NO
   echo -ne "\ek$TITLE\e\\"
+else
+  AUTO_TITLE_SCREENS=YES
 fi
 
 #Will get the zsh_version from eihooks
