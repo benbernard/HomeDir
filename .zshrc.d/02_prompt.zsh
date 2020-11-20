118c0482e700306cfd5c6b46cca4791a49b13340
Ben Bernard <github@benjaminbernard.com>
2020-11-20 11:59:40 -0800

    Setting up powerline

--- .zshrc.d/02_prompt.zsh
+++ .zshrc.d/02_prompt.zsh
@@ -60,63 +60,69 @@ if autoload +X url-quote-magic 2> /dev/null; then
   zle -N self-insert url-quote-magic
 fi
 
-# This prompt uses oh-my-zsh prompt stuff for colors and the git prompt so lets deconstruct this:
-# Really useful reference for the prompt: http://www.nparikh.org/unix/prompt.php
+#  # This prompt uses oh-my-zsh prompt stuff for colors and the git prompt so lets deconstruct this:
+#  # Really useful reference for the prompt: http://www.nparikh.org/unix/prompt.php
+#  #
+#  # Note: using $fg_bold[color] inside a %(x.true.false) statment yields prompts
+#  # with lines longer than the width of the terminal
+#  #
+#  # $''
+#  #   Means to not interpolate environment variables, but do interpolate escape
+#  #   sequences, I believe
+#  #
+#  # %(0?.%{\e[1;32m%}.%{\e[3;31m%})
+#  #   First, lets color the first section of the prompt on the command exit of the
+#  #   previous command %(x.true.false) is the syntax and x=? means the exit code of
+#  #   the previous command
+#  #   The \e[1;32m codes are colors.  1 for bold 32 or 31 for green/red (1 vs 3
+#  #   for foreground vs. background)
+#  # ➜
+#  #   Literal character, unicode.
+#  # %*
+#  #   Current time.  For some reason p, P, Y all don't work.  zsh seems to have
+#  #   some alternatives that do work
+#  # %{$reset_color%}
+#  #   Reset the possible color from the previous command check
+#  # %(4L.S:$SHLVL .)
+#  #   Check the current SHLVL, if it is greater than 3 display S:$SHLVL so I can
+#  #   know if I'm in a sub shell
+#  # %{$fg_bold[blue]%}$(site_prompt_info)%{$fg_bold[blue]%}
+#  #   Color the prompt blue, display the site (machine) specific portion of the
+#  #   prompt with the call to site_prompt_info (which will be defined by
+#  #   99_last.zsh if nowhere else
+#  # $(vi_mode_prompt_info)
+#  #   Only set when in zle command line mode see .oh-my-zsh/plugins/vi-mode for
+#  #   more info, displays $MODE_INDICATOR when in command mode in the prompt
+#  # %%
+#  #   Literal character: %
+#  # %{$reset_color%}'
+#  #   Resets the color back to the original color
+#  PROMPT=$'%(0?.%{\e[1;32m%}.%{\e[3;31m%})➜ %*%{$reset_color%} %(4L.S:$SHLVL .)%{$fg_bold[blue]%}$(git_prompt_info_site)%{$reset_color%}$(vi_mode_prompt_info) %%%{$reset_color%} '
 #
-# Note: using $fg_bold[color] inside a %(x.true.false) statment yields prompts
-# with lines longer than the width of the terminal
+#  setopt TRANSIENT_RPROMPT # RPROMPT disappears in terminal history great for copying
+#  #RPROMPT="%{${fg[$PROMPT_COLOR]}%}%B%(7~,.../,)%6~%b%{${fg[default]}%} $(vi_mode_prompt_info)"
 #
-# $''
-#   Means to not interpolate environment variables, but do interpolate escape
-#   sequences, I believe
+#  # Remove a space
+#  ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%})%{$fg[yellow]%}✗%{$reset_color%}"
 #
-# %(0?.%{\e[1;32m%}.%{\e[3;31m%})
-#   First, lets color the first section of the prompt on the command exit of the
-#   previous command %(x.true.false) is the syntax and x=? means the exit code of
-#   the previous command
-#   The \e[1;32m codes are colors.  1 for bold 32 or 31 for green/red (1 vs 3
-#   for foreground vs. background)
-# ➜
-#   Literal character, unicode.
-# %*
-#   Current time.  For some reason p, P, Y all don't work.  zsh seems to have
-#   some alternatives that do work
-# %{$reset_color%}
-#   Reset the possible color from the previous command check
-# %(4L.S:$SHLVL .)
-#   Check the current SHLVL, if it is greater than 3 display S:$SHLVL so I can
-#   know if I'm in a sub shell
-# %{$fg_bold[blue]%}$(site_prompt_info)%{$fg_bold[blue]%}
-#   Color the prompt blue, display the site (machine) specific portion of the
-#   prompt with the call to site_prompt_info (which will be defined by
-#   99_last.zsh if nowhere else
-# $(vi_mode_prompt_info)
-#   Only set when in zle command line mode see .oh-my-zsh/plugins/vi-mode for
-#   more info, displays $MODE_INDICATOR when in command mode in the prompt
-# %%
-#   Literal character: %
-# %{$reset_color%}'
-#   Resets the color back to the original color
-PROMPT=$'%(0?.%{\e[1;32m%}.%{\e[3;31m%})➜ %*%{$reset_color%} %(4L.S:$SHLVL .)%{$fg_bold[blue]%}$(git_prompt_info_site)%{$reset_color%}$(vi_mode_prompt_info) %%%{$reset_color%} '
+#  #change branch name to yellow, not red
+#  ZSH_THEME_GIT_PROMPT_PREFIX="g:(%{$fg[yellow]%}"
+#
+#  ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%})"
+#
+#  # change final % in prompt into red if in command mode
+#  MODE_INDICATOR="%{$fg_bold[red]%}"
+#
+#  # get the name of the branch we are on
+#  function git_prompt_info_site() {
+#    echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(gitbranch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+#  }
 
-setopt TRANSIENT_RPROMPT # RPROMPT disappears in terminal history great for copying
-#RPROMPT="%{${fg[$PROMPT_COLOR]}%}%B%(7~,.../,)%6~%b%{${fg[default]}%} $(vi_mode_prompt_info)"
+# powerlevel10k prompt setup
+source $(submodule powerlevel10k)/powerlevel10k.zsh-theme
 
-# Remove a space
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%})%{$fg[yellow]%}✗%{$reset_color%}"
-
-#change branch name to yellow, not red
-ZSH_THEME_GIT_PROMPT_PREFIX="g:(%{$fg[yellow]%}"
-
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%})"
-
-# change final % in prompt into red if in command mode
-MODE_INDICATOR="%{$fg_bold[red]%}"
-
-# get the name of the branch we are on
-function git_prompt_info_site() {
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(gitbranch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-}
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 zmodload zsh/zpty 1>/dev/null 2>/dev/null
 if type zpty >/dev/null;
