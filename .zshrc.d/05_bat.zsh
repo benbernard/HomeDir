3ed72ae2d625ab96e5d2db1ccbf13bf72cfd85ad
Ben Bernard <ben.bernard@instacart.com>
2021-02-05 10:12:04 -0800

    Tweak bat setup

--- .zshrc.d/05_bat.zsh
+++ .zshrc.d/05_bat.zsh
@@ -1,5 +1,5 @@
 if command -v bat >/dev/null 2>/dev/null; then
-  set bat as the default pager
+  # set bat as the default pager
   export PAGER=bat
   export BAT_PAGER="less -XRMSIF"
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
