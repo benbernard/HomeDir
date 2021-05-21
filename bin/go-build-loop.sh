bfd56d656d6e816bf26e221df74847ebd2bcc6a6
Ben Bernard <github@benjaminbernard.com>
2021-05-21 12:19:42 -0700

    Move back to go-build-loop

--- bin/go-build-loop.sh
+++ bin/go-build-loop.sh
@@ -3,6 +3,13 @@
 set -u
 set -eo pipefail
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
+local -a MAKE_ARGS=("${@}")
+
 findRoot() {
   root=`git rev-parse --show-toplevel`
   if [[ ! -e ${root}/go.sum ]]; then
@@ -13,28 +20,33 @@ findRoot() {
 }
 
 runTest() {
-  if make "$@"; then
-    echo BUILD SUCCESSFUL
+  echo
+
+  afplay /System/Library/Sounds/Blow.aiff -r 5 -v 0.4 &
+  echo "${YELLOW}==========  Running Tests =========${RESET}"
+  if make "${MAKE_ARGS[@]}"; then
+    echo "${GREEN}==========  BUILD SUCCEEDED =========${RESET}"
     afplay /System/Library/Sounds/Ping.aiff -v 0.4 -r 2 &
   else
+    echo "${RED}==========  BUILD FAILED =========${RESET}"
     say -r 600 -v Moira '[[volm 0.5]] Failed'
   fi
+
+  echo
 }
 
 findRoot
 echo "Found repo ${ROOT_DIR}"
+echo "Using make args ${MAKE_ARGS}"
 
 runTest
 
 while (true) {
   # Wiat for a change
   fswatch -1 ${ROOT_DIR}
-  if make "$@"; then
-    echo BEEP && beep
-  else
-    echo FAILED
-  fi
+  runTest
 
   # Sleep so you can kill
+  echo; echo
   sleep 1
 }
