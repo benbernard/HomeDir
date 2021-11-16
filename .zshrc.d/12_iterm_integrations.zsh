a10d4564b6f4baca76eb6d051579bd60e1517020
Ben Bernard <github@benjaminbernard.com>
2021-11-16 14:59:48 -0800

    Make iterm opitonal

--- .zshrc.d/12_iterm_integrations.zsh
+++ .zshrc.d/12_iterm_integrations.zsh
@@ -1,1 +1,3 @@
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+if [[ ! $IS_REMOTE_INSTANCE -eq "true" ]]; then
+  test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+fi
