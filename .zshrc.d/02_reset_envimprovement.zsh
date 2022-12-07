d187d22e42e86bf4c60fb6cc96e5cd2f451ef3bc
Ben Bernard <ben.bernard@instacart.com>
2022-12-07 10:58:51 -0800

    Some more remote fixes

--- .zshrc.d/02_reset_envimprovement.zsh
+++ .zshrc.d/02_reset_envimprovement.zsh
@@ -1,20 +1,2 @@
 #Do not use the envImprovement SCREENRC, as I want my own rc file
 unset SCREENRC
-
-# Setup for ninja-search shell
-if [[ "$NINJA_SEARCH_SHELL" = "YES" ]]
-then
-  # if we want screen titling stuff, we'll need to grab that from the
-  # envImprovement zshrc, but I don't care enough for the ninja-search shell
-  precmd () {
-    # Need nasty backgrounding process in order to prevent a rogue % from messing up the prompt
-    sh -c "screen -X stuff 'ns '" &!
-  }
-
-  preexec () { }
-
-  RPROMPT=
-  PROMPT=
-
-  HISTFILE=~/.history.ninja-search
-fi
