46dc904f270eb5d101313049da840c3d4c63325a
Ben Bernard <ben.bernard@instacart.com>
2023-12-12 09:09:54 -0800

    Disable iterm integration

--- .zshrc.d/12_iterm_integrations.zsh
+++ .zshrc.d/12_iterm_integrations.zsh
@@ -1,7 +1,7 @@
 ITERM_SHELL_INTEG_FILE=${HOME}/.iterm2_shell_integration.zsh
 
-if [ -e ${ITERM_SHELL_INTEG_FILE} ]; then
-  echo Running shell integ
-  export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=1
-  source ${ITERM_SHELL_INTEG_FILE}
-fi
+# if [ -e ${ITERM_SHELL_INTEG_FILE} ]; then
+#   echo Running shell integ
+#   export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=1
+#   source ${ITERM_SHELL_INTEG_FILE}
+# fi
