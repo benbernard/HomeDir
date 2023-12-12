cf40d891cc4ef85629327e81ec283f414445a6ed
Ben Bernard <ben.bernard@instacart.com>
2023-12-12 09:10:33 -0800

    Add ava shell

---
+++ .zshrc.d/15_ava_shell.zsh
@@ -0,0 +1,15 @@
+ava-shell () {
+  local tmpfile=$(mktemp);
+  trap 'rm -f $tmpfile' EXIT;
+  if ~/repos/bento/bentodev ava shell "$@" --result-file $tmpfile; then
+    if [ -e "$tmpfile" ]; then
+      local fixed_cmd=$(cat $tmpfile);
+      print -z "$fixed_cmd";
+    else
+      echo "Apologies! Extracting command failed"
+    fi
+  else
+    return 1
+  fi
+};
+alias '?a'='ava-shell';
