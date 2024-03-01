755d82a5532e0f65e56fcb2c9db7f718b0168eb8
Ben Bernard <ben.bernard@instacart.com>
2024-03-01 10:17:51 -0800

    Update copilot paths

--- .zshrc.d/14_copilot.zsh
+++ .zshrc.d/14_copilot.zsh
@@ -2,7 +2,7 @@
   copilot_what-the-shell () {
     TMPFILE=$(mktemp);
     trap 'rm -f $TMPFILE' EXIT;
-    if /Users/benbernard/.nvm/versions/node/v18.12.1/bin/github-copilot-cli what-the-shell "$@" --shellout $TMPFILE; then
+    if /opt/homebrew/bin/github-copilot-cli what-the-shell "$@" --shellout $TMPFILE; then
       if [ -e "$TMPFILE" ]; then
         FIXED_CMD=$(cat $TMPFILE);
         print -z "$FIXED_CMD";
@@ -19,7 +19,7 @@ alias '??'='copilot_what-the-shell';
   copilot_git-assist () {
     TMPFILE=$(mktemp);
     trap 'rm -f $TMPFILE' EXIT;
-    if /Users/benbernard/.nvm/versions/node/v18.12.1/bin/github-copilot-cli git-assist "$@" --shellout $TMPFILE; then
+    if /opt/homebrew/bin/github-copilot-cli git-assist "$@" --shellout $TMPFILE; then
       if [ -e "$TMPFILE" ]; then
         FIXED_CMD=$(cat $TMPFILE);
         print -z "$FIXED_CMD";
@@ -36,7 +36,7 @@ alias 'git?'='copilot_git-assist';
   copilot_gh-assist () {
     TMPFILE=$(mktemp);
     trap 'rm -f $TMPFILE' EXIT;
-    if /Users/benbernard/.nvm/versions/node/v18.12.1/bin/github-copilot-cli gh-assist "$@" --shellout $TMPFILE; then
+    if /opt/homebrew/bin/github-copilot-cli gh-assist "$@" --shellout $TMPFILE; then
       if [ -e "$TMPFILE" ]; then
         FIXED_CMD=$(cat $TMPFILE);
         print -z "$FIXED_CMD";
