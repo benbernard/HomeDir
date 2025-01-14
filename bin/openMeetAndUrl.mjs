f9e3515ae220b738469b0104f948842f119b0c75
Ben Bernard <ben.bernard@instacart.com>
2025-01-14 11:00:58 -0800

    Update debug printing for openMeet

--- bin/openMeetAndUrl.mjs
+++ bin/openMeetAndUrl.mjs
@@ -1,58 +1,66 @@
-#!/opt/homebrew/bin/node
+#!/usr/local/bin/node
 
-import { spawnSync } from 'child_process';
+import { spawnSync } from "node:child_process";
+
+const DEBUG = false;
 
 function openGoogleMeet() {
-  const command = 'open'
-  const args = [
-    '-a',
-    '/Users/benbernard/Applications/Chrome Apps.localized/Google Meet.app'
-  ]
+	const command = "open";
+	const args = [
+		"-a",
+		"/Users/benbernard/Applications/Chrome Apps.localized/Google Meet.app",
+	];
 
-  // console.log(`Running ${command} ${args.join(' ')}`);
-  spawnSync(command, args, { stdio: 'inherit' })
-  spawnSync('sleep', [2], { stdio: 'inherit' })
+	if (DEBUG) {
+		console.log(`Running ${command} ${args.join(" ")}`);
+	}
+
+	spawnSync(command, args, { stdio: "inherit" });
+	spawnSync("sleep", [2], { stdio: "inherit" });
 }
 
 function openUrl(url) {
-  const command = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-  const args = [
-    '-g',
-    '--args',
-    '--profile-directory=Default',
-    url
-  ]
+	const command =
+		"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+	const args = ["-g", "--args", "--profile-directory=Default", url];
 
-  // console.log(`Running ${command} ${args.join(' ')}`);
-  spawnSync(command, args, { stdio: 'inherit' })
+	if (DEBUG) {
+		console.log(`Running ${command} ${args.join(" ")}`);
+	}
+
+	spawnSync(command, args, { stdio: "inherit" });
 }
 
 // Find if the Google Meet.app is running
 function isGoogleMeetRunning() {
-  const command = 'ps'
-  const args = [
-    '-ax',
-    '-o',
-    'command'
-  ]
+	const command = "ps";
+	const args = ["-ax", "-o", "command"];
 
-  const result = spawnSync(command, args, { stdio: 'pipe' })
-  const output = result.stdout.toString()
+	if (DEBUG) {
+		console.log(`Running ${command} ${args.join(" ")}`);
+	}
 
-  return output.includes('Google Meet.app/Contents/MacOS/app_mode_loader')
+	const result = spawnSync(command, args, { stdio: "pipe" });
+	const output = result.stdout.toString();
+
+	if (DEBUG) {
+		console.log(`Output: ${output}`);
+	}
+
+	return output.includes("Google Meet.app/Contents/MacOS/app_mode_loader");
 }
 
 function main() {
-  if (!isGoogleMeetRunning()) {
-    openGoogleMeet();
-  }
+	if (!isGoogleMeetRunning()) {
+		openGoogleMeet();
+	}
 
-  if (process.argv.length < 3) {
-    console.log('Usage: openMeetAndUrl <url>')
-    process.exit(1)
-  }
+	if (process.argv.length < 3) {
+		console.log("Usage: openMeetAndUrl <url>");
+		process.exit(1);
+	}
 
-  openUrl(process.argv[2])
+	openUrl(process.argv[2]);
 }
 
 main();
